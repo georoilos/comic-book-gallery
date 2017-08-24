4290d8a22e4f9e6893c4dbfda07c9c2a7fb27bbb
George <georoilos@yahoo.com>
2017-08-25 00:54:26 +0100

    Action Result - Added

---
+++ src/ComiCBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComiCBookGallery.Controllers
+{
+	public class ComicBooksController: Controller
+	{
+			public ActionResult Detail()
+			{
+				if(DateTime.Today.DayOfWeek == DayOfWeek.Friday)
+				{
+					return new RedirectResult("/");
+				}
+					else return new ContentResult()
+				{
+					Content = "Hello from Controller"
+				};
+
+			}
+
+	}
+}
